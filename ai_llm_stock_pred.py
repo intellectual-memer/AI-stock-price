3d5ee2c9f1d98daae85f3dd5fb9b48766def3839
intellectual-memer <preeteshshivam91@gmail.com>
2024-08-22 15:21:11 -0400

    Update ai_llm_stock_pred.py

--- ai_llm_stock_pred.py
+++ ai_llm_stock_pred.py
@@ -31,9 +31,12 @@ import streamlit as st
 # Connect to local Ollama server
 #llm = Ollama(model='llama3')
 llm = Ollama(
-    base_url='http://localhost:11434',
+    #base_url='http://localhost:11434',
+    base_url='http://127.0.0.1:11434',
+    #base_url='http://192.168.1.151:11434',
     model="llama3"
 )
+print(ollama.invoke("why is the sky blue"))
 
 ##############
 ### PART 2 ###
